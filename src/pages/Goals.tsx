03ec808a6bd344d2c533430fd04251b7ba6df524
smashupbuddys <68978773+smashupbuddys@users.noreply.github.com>
2025-03-18 17:21:18 +0530

    unzipgit

---
+++ src/pages/Goals.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinancialGoals from '../components/goals/FinancialGoals';
+
+const Goals: React.FC = () => {
+  return (
+    <div>
+      <FinancialGoals />
+    </div>
+  );
+};
+
+export default Goals;
